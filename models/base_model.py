0e577d0ab6ff4bdcfb4d4234853d21c86ab6cbad
Odarell35 <leratomawash0@gmail.com>
2023-08-08 07:35:04 -0700

    run task 3

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
 
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.creted_at = datetime.now()
+            self.created_at = datetime.now()
             self.updated_at = datetime.now()
             self.save()
         else:
@@ -28,7 +28,7 @@ class BaseModel:
     
     def __str__(self):
         """string representation of an object"""
-        return "[{s}] ({s}) {s}" .format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
@@ -45,4 +45,3 @@ class BaseModel:
         update public update at current time instance
         '''
         self.updated_at = datetime.now()
-        storage,save
