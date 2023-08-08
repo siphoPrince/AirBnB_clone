f73932acf42fd64a6b31c82f6f0aa5e200911e59
Odarell35 <leratomawash0@gmail.com>
2023-08-08 08:01:05 -0700

    made changes on the to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.save()
+            #self.save()
         else:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%M-%dT%H:%M:%S.%f')
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%M-%dT%H:%M:%S.%f')
@@ -30,18 +30,19 @@ class BaseModel:
         """string representation of an object"""
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-    def to_dict(self):
-        """returns a dictionary containing all keys/values
-        of __dict__ of the instance"""
-        dictionary = self.__dict__
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = dictionary['created_at'].isoformat
-        dictionary['updated_at'] = dictionary['updated_at'].isoformat
-
-        return dictionary
-            
     def save(self):
         '''
         update public update at current time instance
         '''
         self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        dictionary = self.__dict__
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dictionary['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+        return dictionary
+            
