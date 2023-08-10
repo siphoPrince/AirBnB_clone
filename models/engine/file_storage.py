6cf6d8435f4059e7af14bcc3878f7c0d6be9ccb8
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-08-10 17:25:55 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,8 +37,8 @@ class FileStorage:
         my_dict = {}
         my_dict.update(FileStorage.__objects)
         for key, value in my_dict.items():
-            my_dict[key] = value.to_dict()
-             #  my_dict[key] = FileStorage.__objects[key].to_dict()
+            #my_dict[key] = value.to_dict()
+             my_dict[key] = FileStorage.__objects[key].to_dict()
         with open(FileStorage.__file_path, "w+") as write_file:
             json.dump(my_dict, write_file)
 
