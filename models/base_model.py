78729bda164fda3976448269436af0b1c9284d2a
Odarell35 <leratomawash0@gmail.com>
2023-08-10 10:19:33 -0700

    changes in the to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -24,16 +24,11 @@ class BaseModel:
             self.updated_at = datetime.now()
             models.storage.new(self)
             models.storage.save()
-        
+      
         else:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%M-%dT%H:%M:%S.%f')
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%M-%dT%H:%M:%S.%f')
             
-            for i, j in kwargs.items():
-                if "__class__" not in i:
-                    setattr(self, i, j)
-
-    
     def __str__(self):
         """string representation of an object"""
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -48,7 +43,7 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all keys/values
         of __dict__ of the instance"""
-        dictionary = self.__dict__
+        dictionary = dict(self.__dict__)
         dictionary['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         dictionary['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         dictionary['__class__'] = self.__class__.__name__
