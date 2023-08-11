5084bd91fadf5bc7a1f6351a151a586becfa2855
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-08-11 10:13:36 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" __ini__ module"""
+""" __init__ module"""
 
 from models.engine.file_storage import FileStorage
 from .base_model import BaseModel
+
 storage = FileStorage()
 storage.reload()
