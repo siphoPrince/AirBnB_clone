de50bcb7810191701ca2915da504e1892a00a26f
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-08-12 18:59:38 +0200

    import module

--- models/base_model.py
+++ models/base_model.py
@@ -17,12 +17,12 @@ class BaseModel:
         '''
         constructor
         '''
-
+        from models import storage
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         
         else:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,12 @@ serialized and deserialized instances to a json file
 '''
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
