7744ba9f7bc8173e0e9268e81832e4aebd25d4fe
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-08-12 17:21:19 +0200

    test cases

---
+++ tests/test_review.py
@@ -0,0 +1,39 @@
+"""Test case for Review Class"""
+import models
+import unittest
+from models import Review
+from models import BaseModel
+
+class TestReview(unittest, TestCase):
+    """Test case for Review Class"""
+    def test_inheritance(self):
+        """test if class was able to innherit BaseModel"""
+        review = Review()
+        self.assertIsInstance(review, BaseModel)
+        self.assertTrue(hasattr(review, "id"))
+        self.assertTrue(hasattr(review, "created_at"))
+        self.assertTrue(hasattr(review, "updated_at"))
+
+    def test_attr(self):
+        """test if attributes were created """
+        review = Review()
+        self.assertTrue(hasattr(review, "place_id"))
+        self.assertTrue(hasattr(review, "user_id"))
+        self.assertTrue(hasattr(review, "text"))
+
+    def test_attr_value(self):
+        """test if attribute value"""
+        review = Review()
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
+
+    def tst_att_type(self):
+        """test the type of value"""
+        review = Review()
+        self.assertTrue(type(review.place_id) == str)
+        self.assertTrue(type(review.user_id) == str)
+        self.assertTrue(type(review.text) == str)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_state.py
@@ -0,0 +1,29 @@
+"""Test case for State class"""
+import unittest
+import models
+from models import State
+from models import BaseModel
+
+class TestState(unittest, TestCase):
+    """Test case for state classes """
+    def test_inheritance(self):
+        """to test whether it inherits from BaseModel"""
+        state = State()
+        self.assertIsInstance(state, BaseModel)
+        self.assertTrue(hasattr(state, "id"))
+        self.assertTrue(hasattr(state, "created_at"))
+        self.assertTrue(hasattr(state, "update_at"))
+
+    def test_attributtes(self):
+        """to test whether attributes are present"""
+        state = State()
+        self.assertTrue(hasattr(state, "name"))
+        self.assertEqual(state.name, "")
+
+    def test_to_dict(self):
+        """test to dict method on state"""
+        state = State()
+        self.assertEqual(type(state.to_dict), dict)
+        
+if __name__ == "__main__":
+    unittest.main()
