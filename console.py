40ebe5bdcc058f4a617500debe3128cff212d989
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-08-14 12:57:56 +0200

    modifications to all ()

--- console.py
+++ console.py
@@ -105,9 +105,9 @@ class HBNBCommand(cmd.Cmd):
                 # new_list.append(str(value))
         storage_list = storage.all()
         for instance in storage_list:
-                print(storage_list[instance])
-            #if (args == key.to_dict()["__class__"]):
-                #print(key)
+            print("first item: {}".format(storage_list[instance]))
+            if args == instance:
+                new_list.append(str(instance))
                 print(new_list)
 
     def do_update(self, args):
