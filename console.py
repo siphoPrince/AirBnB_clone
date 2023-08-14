9b8668d44694bcb41d28864cda2e8bc8fb89cb6a
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-08-14 13:23:12 +0200

    condition met

--- console.py
+++ console.py
@@ -106,7 +106,7 @@ class HBNBCommand(cmd.Cmd):
         storage_list = storage.all()
         for instance in storage_list:
             #print("first item: {}".format(storage_list[instance]))
-            new_list.append(str(storage_list[instance]))
+            #new_list.append(str(storage_list[instance]))
             #print(new_list)
             if (args == storage_list[instance].to_dict()["__class__"]):
                 new_list.append(str(storage_list[instance]))
