9b9ca8df81a68b73ec945506dee514ff67ff9313
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-08-14 12:41:15 +0200

    do all

--- console.py
+++ console.py
@@ -100,14 +100,15 @@ class HBNBCommand(cmd.Cmd):
         if args and args not in ClassDict:
             print("** class doesn't exist **")
             return
-        #dic = storage.all()
-        #print(dic)
+        
         for key, value in storage.all().items():
             if not args or key.split(".")[0] == args:
                 new_list.append(str(value))
             #if (args == key.to_dict()["__class__"]):
                 #print(key)
                 print(new_list)
+            else:
+                print("basemodel not found")
 
     def do_update(self, args):
         """Update an instance attribute and save the change"""
